1720b1623e4b73729cab7cd25a97f71679cf4c3b
camilaMartin14 <camilamartin1403@gmail.com>
2025-09-03 17:06:27 -0300

    limpiar service

--- Practica01/Services/InvoiceService.cs
+++ Practica01/Services/InvoiceService.cs
@@ -11,48 +11,6 @@ namespace Practica01.Servicios
 {
     public class InvoiceService
     {
-        //el service guarda la lógica del negocio: qué se puede hacer, cómo, con qué límites,
-        //y de qué forma se conectan la interfaz y los datos.
-        private IInvoiceRepository _invoiceRepository;
-
-        public InvoiceService()
-        {
-            _invoiceRepository = new InvoiceRepository();
-        }
-
-        public Invoice GetInvoiceById(int id)
-        {
-            return _invoiceRepository.GetById(id);
-        }
-
-        public List<Invoice> GetAll()
-        {
-            return _invoiceRepository.GetAll();
-        }
-
-        /*
-        public bool SaveInvoice(Invoice invoice)
-        {
-            bool saved;
-
-            var SavedInvoice = _invoiceRepository.GetById(invoice.Number);
-
-            if (SavedInvoice == null)
-            {
-                saved = _invoiceRepository.Save(invoice);
-            }
-            else
-            {
-                saved = false;
-            }
-            return saved;
-        }
-        */
-
-        public bool SaveInvoice(Invoice invoice)
-        {
-            // Guardamos la factura directamente, sin chequear Number
-            return _invoiceRepository.Save(invoice);
-        }
+        
     }
 }
